aac8564ec32f4ea56a4aa99c775efb3a68966a5b
Georges Kesseler <georgeskesseler@yahoo.com>
2019-11-25 21:21:33 +0100

    added crude disassembly

---
+++ disassembly/README.md
@@ -0,0 +1,15 @@
+# Disassembly
+thanks to Tronic Effect
+
+
+
+### If you want to reduce the size of the code file:
+
+It is possible to assemble the data files to a file in devpac for each decompiled data set and then use an incbin for this file replacing the disassembled data set data  
+    
+ If that makes sense!!
+
+ This will make the code smaller without data files decompiled and added to the code by the disassembler
+
+ Cut and paste the decompiled data files out of the code, compile as data in devpac, replace decompiled data in the code with incbin of compiled data file extract
+
--- dominokomp/mkdomino.py
+++ dominokomp/mkdomino.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python
-used=[]
-domino=[]
+used=[0]*256
+domino=[None]*257
+
+# recursive function doing stuff
+founddomino=0
+def mkdomino(last,level):
+  global founddomino
+  global used
+  global domino
+  print "Level:",level
+  if(level<=0xff):
+    # print "Enter..."
+    domino[level]=hex(last)
+    for i in range(last*0x10,last*0x10+0xf):
+      # print "loop ",i
+      if(used[i]==0):
+        used[i]=1
+        mkdomino(i % 0x10,level+1)
+        used[i]=0
+      if (founddomino==1):
+        print "exiting on 1"
+        return
+    # next i
+  else: 
+    founddomino=1
 
 used[0]=1
 print "0",
@@ -11,22 +34,8 @@ domino[0x100]=domino[0]
 
 for i in range(0,0xff):
   if((i % 16) == 0):
+    print ""
     print " dc.w ",
-  print hex(i),
+  print domino[i],
   print ",",
-
-# recursive function doing stuff
-def mkdomino(last,level):
-  if(level<=0xff):
-    domino[level]=hex(last)
-    for i in range(last*0x10,last*0x10+0xf):
-      if(used[i]==0):
-        used[i]=1
-        mkdomino(i % 0x10,level+1)
-        used[i]=0
-      if (founddomino==1):
-        return
-    # next i
-    else: 
-      founddomino=1
 
